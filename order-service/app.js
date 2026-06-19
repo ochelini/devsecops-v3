7b3f28c63eeb07d725efa88b10c2f1f4a0bb9b28
ochelini <ochelini@gmail.com>
2026-06-18 21:47:33 -0300

    day 2 complete - dockerized services

--- order-service/app.js
+++ order-service/app.js
@@ -5,4 +5,4 @@ app.get('/orders', (req, res) => {
   res.send('Order service running');
 });
 
-app.listen(8080, () => console.log('Order service running'));
+app.listen(8080, () => console.log('Order service running inside container'));
--- user-service/app.js
+++ user-service/app.js
@@ -5,4 +5,5 @@ app.get('/users', (req, res) => {
   res.send('User service running');
 });
 
-app.listen(8080, () => console.log('User service running'));
+app.listen(8080, () => console.log('User service running inside container'));
+``
